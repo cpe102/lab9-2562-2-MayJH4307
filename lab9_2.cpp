85fd9f04ec068b6a7c9593ee3a9e7dabffce9b55
MayJh4307 <norit.hunhan@hotmail.com>
2020-01-28 16:36:15 +0700

    finished-2

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -2,3 +2,26 @@
 //cout << textline;
 //cout << "-------------------- SOTUS ---------------------";
 //Don't forget to change cout to output filestream
+#include<iostream>
+#include<fstream>
+#include<string>
+using namespace std;
+
+int main(){
+    ifstream cheerbook;
+    cheerbook.open("C:\\Users\\admin\\Documents\\ComPro\\LABinClass\\lab9-2562-2-MayJH4307\\cheerbook.txt");
+    string textline;
+    ofstream cheerbook_copy;
+    cheerbook_copy.open("C:\\Users\\admin\\Documents\\ComPro\\LABinClass\\lab9-2562-2-MayJH4307\\cheerbook_copy.txt");
+    cheerbook_copy << "-------------------- SOTUS ---------------------" << endl;
+    
+    while(getline(cheerbook,textline)){
+         cheerbook_copy << textline << "\n";
+    }
+    cheerbook_copy << "-------------------- SOTUS ---------------------" << endl;
+    cheerbook.close();
+    cheerbook_copy.close();
+    return 0;    
+
+}
+
